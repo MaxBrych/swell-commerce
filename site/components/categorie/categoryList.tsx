b137516fa2d5c7eb76e2aceb039c87899dc183da
MaxBrych <max.brych03@gmail.com>
2023-03-13 22:41:47 +0100

    categories section

--- site/components/categorie/categoryList.tsx
+++ site/components/categorie/categoryList.tsx
@@ -1,132 +1,149 @@
-import { FC } from 'react'
 import cn from 'clsx'
+import type { SearchPropsType } from '@lib/search-props'
 import Link from 'next/link'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+
 import type { Product } from '@commerce/types/product'
-import s from '../product/ProductCard/ProductCard.module.css'
-import Image, { ImageProps } from 'next/image'
-import WishlistButton from '@components/wishlist/WishlistButton'
-import usePrice from '@framework/product/use-price'
-import ProductTag from '../product/ProductTag'
 
-interface Props {
-  className?: string
-  product: Product
-  noNameTag?: boolean
-  imgProps?: Omit<ImageProps, 'src' | 'layout' | 'placeholder' | 'blurDataURL'>
-  variant?: 'default' | 'slim' | 'simple'
-}
+import {
+  filterQuery,
+  getCategoryPath,
+  getDesignerPath,
+  useSearchMeta,
+} from '@lib/search'
+import { Container } from '@components/ui'
+import { Layout } from '@components/common'
+import ErrorMessage from '@components/ui/ErrorMessage'
 
-const placeholderImg = '/product-img-placeholder.svg'
+export default function Categories({ categories }: SearchPropsType) {
+  const [activeFilter, setActiveFilter] = useState('')
+  const [toggleFilter, setToggleFilter] = useState(false)
 
-const Categories: FC<Props> = ({
-  product,
-  imgProps,
-  className,
-  noNameTag = false,
-  variant = 'default',
-}) => {
-  const { price } = usePrice({
-    amount: product.price.value,
-    baseAmount: product.price.retailPrice,
-    currencyCode: product.price.currencyCode!,
-  })
+  const router = useRouter()
+  const { asPath, locale } = router
+  const { q, sort } = router.query
+  // `q` can be included but because categories and designers can't be searched
+  // in the same way of products, it's better to ignore the search input if one
+  // of those is selected
+  const query = filterQuery({ sort })
 
-  const rootClassName = cn(
-    s.root,
-    { [s.slim]: variant === 'slim', [s.simple]: variant === 'simple' },
-    className
-  )
+  const { pathname, category, brand } = useSearchMeta(asPath)
+
+  const activeCategory = categories.find((cat: any) => cat.slug === category)
+
+  const handleClick = (event: any, filter: string) => {
+    if (filter !== activeFilter) {
+      setToggleFilter(true)
+    } else {
+      setToggleFilter(!toggleFilter)
+    }
+    setActiveFilter(filter)
+  }
 
   return (
-    <Link
-      href={`/product/${product.slug}`}
-      className={rootClassName}
-      aria-label={product.name}
-    >
-      {variant === 'slim' && (
-        <>
-          <div className={s.header}>
-            <span>{product.name}</span>
-          </div>
-          {product?.images && (
-            <Image
-              quality="85"
-              src={product.images[0]?.url || placeholderImg}
-              alt={product.name || 'Product Image'}
-              height={320}
-              width={320}
-              {...imgProps}
-            />
-          )}
-        </>
-      )}
-
-      {variant === 'simple' && (
-        <>
-          {process.env.COMMERCE_WISHLIST_ENABLED && (
-            <WishlistButton
-              className={s.wishlistButton}
-              productId={product.id}
-              variant={product.variants[0]}
-            />
-          )}
-          <div className={s.imageContainer}>
-            {product?.images && (
-              <Image
-                alt={product.name || 'Product Image'}
-                className={s.productImage}
-                src={product.images[0]?.url || placeholderImg}
-                object-cover
-                object-center
-                quality="85"
-                {...imgProps}
-              />
-            )}
-          </div>
-          {!noNameTag && (
-            <div className={s.header}>
-              <h3>
-                <span>{product.name}</span>
-              </h3>
-              <p>{product.description}</p>
-              <div className={s.price}>
-                {`${price} ${product.price?.currencyCode}`}
-              </div>
+    <Container>
+      {/* Categories */}
+      <div className="relative inline-block w-full">
+        <div className="lg:hidden">
+          <span className="rounded-md shadow-sm">
+            <button
+              type="button"
+              onClick={(e) => handleClick(e, 'categories')}
+              className="flex justify-between w-full px-4 py-3 text-sm font-medium leading-5 transition duration-150 ease-in-out border rounded-sm border-accent-3 bg-accent-0 text-accent-4 hover:text-accent-5 focus:outline-none focus:border-blue-300 focus:shadow-outline-normal active:bg-accent-1 active:text-accent-8"
+              id="options-menu"
+              aria-haspopup="true"
+              aria-expanded="true"
+            >
+              {activeCategory?.name
+                ? `Category: ${activeCategory?.name}`
+                : 'All Categories'}
+              <svg
+                className="w-5 h-5 ml-2 -mr-1"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </span>
+        </div>
+        <div
+          className={`origin-top-left absolute lg:relative left-0 mt-2 w-full rounded-md shadow-lg lg:shadow-none z-10 mb-10 lg:block ${
+            activeFilter !== 'categories' || toggleFilter !== true
+              ? 'hidden'
+              : ''
+          }`}
+        >
+          <div className="rounded-sm shadow-xs bg-accent-0 lg:bg-none lg:shadow-none">
+            <div
+              role="menu"
+              aria-orientation="vertical"
+              aria-labelledby="options-menu"
+            >
+              <ul>
+                <li
+                  className={cn(
+                    'block text-sm leading-5 text-accent-4 lg:text-base lg:no-underline lg:font-bold lg:tracking-wide hover:bg-accent-1 lg:hover:bg-transparent hover:text-accent-8 focus:outline-none focus:bg-accent-1 focus:text-accent-8',
+                    {
+                      underline: !activeCategory?.name,
+                    }
+                  )}
+                >
+                  <Link
+                    href={{ pathname: getCategoryPath('', brand), query }}
+                    legacyBehavior
+                  >
+                    <a
+                      onClick={(e) => handleClick(e, 'categories')}
+                      className={
+                        'block lg:inline-block px-4 py-2 lg:p-0 lg:my-2 lg:mx-4'
+                      }
+                    >
+                      All Categories
+                    </a>
+                  </Link>
+                </li>
+                {categories.map((cat: any) => (
+                  <li
+                    key={cat.path}
+                    className={cn(
+                      'block text-sm leading-5 text-accent-4 hover:bg-accent-1 lg:hover:bg-transparent hover:text-accent-8 focus:outline-none focus:bg-accent-1 focus:text-accent-8',
+                      {
+                        underline: activeCategory?.id === cat.id,
+                      }
+                    )}
+                  >
+                    <Link
+                      href={{
+                        pathname: getCategoryPath(cat.path, brand),
+                        query,
+                      }}
+                      legacyBehavior
+                    >
+                      <a
+                        onClick={(e) => handleClick(e, 'categories')}
+                        className={
+                          'block lg:inline-block px-4 py-2 lg:p-0 lg:my-2 lg:mx-4'
+                        }
+                      >
+                        {cat.name}
+                      </a>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
             </div>
-          )}
-        </>
-      )}
-
-      {variant === 'default' && (
-        <>
-          {process.env.COMMERCE_WISHLIST_ENABLED && (
-            <WishlistButton
-              className={s.wishlistButton}
-              productId={product.id}
-              variant={product.variants[0] as any}
-            />
-          )}
-          <ProductTag
-            name={product.name}
-            price={`${price} ${product.price?.currencyCode}`}
-          />
-          <div className={s.imageContainer}>
-            {product?.images && (
-              <Image
-                alt={product.name || 'Product Image'}
-                className={s.productImage}
-                src={product.images[0]?.url || placeholderImg}
-                height={540}
-                width={540}
-                quality="85"
-                {...imgProps}
-              />
-            )}
           </div>
-        </>
-      )}
-    </Link>
+        </div>
+      </div>
+    </Container>
   )
 }
 
-export default Categories
+Categories.Layout = Layout
--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -9,6 +9,8 @@ import { Grid, Marquee, Hero } from '@components/ui'
 import type { GetStaticPropsContext, InferGetStaticPropsType } from 'next'
 import { useEffect, useState } from 'react'
 import Categories from '@components/categorie/categoryList'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export async function getStaticProps({
   preview,
@@ -52,6 +54,7 @@ export const revalidate = 30
 
 export default function Home({
   products,
+  categories,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   const [documents, setDocuments] = useState(null)
   const [posts, setPosts] = useState([])
@@ -68,14 +71,32 @@ export default function Home({
     <>
       <div className="px-4 ">
         <div className="my-6">
-          <div className="h-[66vh] bg-[url('https://cdn.discordapp.com/attachments/1084536803157090514/1084563027917033522/Stage_Bledner.png')]  bg-no-repeat bg-cover bg-center bg-fixed rounded-3xl"></div>
+          <div className="h-[66vh] bg-[url('https://cdn.discordapp.com/attachments/1084536803157090514/1084563027917033522/Stage_Bledner.png')]  bg-no-repeat bg-cover bg-center bg-fixed rounded-3xl">
+            <div className="flex flex-col items-center justify-center h-full gap-5">
+              <h1 className="text-4xl font-bold text-center text-white">
+                Hier findest du <br /> was du suchst
+              </h1>
+              <button className="flex items-center justify-center h-12 p-8 bg-orange-500 rounded-full">
+                Zum Shop
+              </button>
+            </div>
+          </div>
         </div>
-
+        <div className="grid grid-cols-6">
+          {categories.map((categorie: any) => (
+            <Link href={`/search/${categorie.slug}`}>
+              <Image src={categorie.image} alt={''} width={32} height={32} />
+              <div className="py-2 text-2xl font-semibold">
+                {categorie.name}
+              </div>
+            </Link>
+          ))}
+        </div>
         <div className="py-2 text-2xl font-semibold">Beliebteste Produkte</div>
         <div className="flex w-full gap-4 overflow-x-scroll md:grid md:overflow-hidden md:grid-cols-4 ">
           {products.slice(0, 4).map((product: any, i: number) => (
             <ProductCard
-              className="flex-shrink-0 lg:w-full "
+              className="flex-shrink-0 lg:w-full md:flex-1 "
               variant="simple"
               key={product.id}
               product={product}
@@ -98,6 +119,7 @@ export default function Home({
           headline="Wir kümmern uns um ihre Technik"
           description="Im Müritzphone Shop Röbel, finden Sie alles was Sie für Ihr Smartphone, Tablet oder Notebook benötigen. Wir bieten Ihnen eine große Auswahl an Zubehör, Ersatzteilen und Reparaturen."
         />
+        {/*
         <Grid layout="B" variant="filled">
           {products.slice(0, 3).map((product: any, i: number) => (
             <ProductCard
@@ -111,17 +133,25 @@ export default function Home({
             />
           ))}
         </Grid>
-
+        
         <Marquee>
           {products.slice(3).map((product: any, i: number) => (
             <ProductCard key={product.id} product={product} variant="slim" />
           ))}
         </Marquee>
-        {/* <HomeAllProductsGrid
+         <HomeAllProductsGrid
         newestProducts={products}
         categories={categories}
         brands={brands}
       /> */}
+        <iframe
+          width="600"
+          height="450"
+          className=""
+          loading="lazy"
+          src="https://www.google.com/maps/embed/v1/place?key=API_KEY
+    &q=Space+Needle,Seattle+WA"
+        ></iframe>
       </div>
     </>
   )
