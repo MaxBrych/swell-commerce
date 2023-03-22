f8c9a7ec1f4a3ffe2d4ede7970c16aa419900b8c
MaxBrych <max.brych03@gmail.com>
2023-03-22 21:46:14 +0100

    added key prop

--- site/components/kunden/Kunden.tsx
+++ site/components/kunden/Kunden.tsx
@@ -8,7 +8,10 @@ const Kunden: FC<Props> = ({ kunden }: Props) => {
     <>
       <div className="flex gap-6 py-12 overflow-x-auto md:py-16 md:grid md:grid-cols-4">
         {kunden.map((kunde) => (
-          <div className="w-[50vw]  md:w-auto border border-gray-300 rounded-xl p-4 flex flex-col justify-start h-[30vh]">
+          <div
+            key={kunde._id}
+            className="w-[50vw]  md:w-auto border border-gray-300 rounded-xl p-4 flex flex-col justify-start h-[30vh]"
+          >
             <p className="text-base md:text-xl">{kunde.kommentar}</p>
             <h5 className="text-xs font-semibold md:text-base">{kunde.name}</h5>
           </div>
