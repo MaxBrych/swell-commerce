4ba4a506bdddad5265aa880445c3349dd960f33f
MaxBrych <max.brych03@gmail.com>
2023-06-23 23:03:53 +0200

    new value images

--- site/components/sections/values/valueData.ts
+++ site/components/sections/values/valueData.ts
@@ -1,24 +1,24 @@
 const ValueData = [
   {
     id: 1,
-    label: 'Gut beraten',
-    descr: 'Wir reparieren Ihr Gerät schnell und zuverlässig',
+    label: 'Vorort',
+    descr: 'Wir sind lokal für Sie da, wenn Sie uns brauchen',
     image:
-      'https://cdn.discordapp.com/attachments/911669935363752026/1094518430637510716/Frame_27.png',
+      'https://cdn.discordapp.com/attachments/911904116782026772/1121903031508811817/3D_Icons3-1440x890_2.png',
   },
   {
     id: 2,
     label: 'Sicherheit',
-    descr: 'Wir reparieren Ihr Gerät schnell und zuverlässig',
+    descr: 'Ihre Daten sind bei uns sicher und werden nicht weitergegeben',
     image:
-      'https://cdn.discordapp.com/attachments/911669935363752026/1094518430419386479/Frame_26.png',
+      'https://cdn.discordapp.com/attachments/911904116782026772/1121903032037290065/safety.png',
   },
   {
     id: 3,
     label: 'Vertrauen',
     descr: 'Wir reparieren Ihr Gerät schnell und zuverlässig',
     image:
-      'https://cdn.discordapp.com/attachments/911669935363752026/1094518430171942982/Frame_28.png',
+      'https://cdn.discordapp.com/attachments/911904116782026772/1121903031777239060/3D_Icons3-1440x890.png',
   },
 ]
 
